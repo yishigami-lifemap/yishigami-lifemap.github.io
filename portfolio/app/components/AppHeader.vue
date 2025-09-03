7eeff06fc5dfe370752610a34b80596a60f261e0
yishigami-lifemap <124244889+yishigami-lifemap@users.noreply.github.com>
2025-09-03 15:03:56 +0900

    galleryページの追加

--- portfolio/app/components/AppHeader.vue
+++ portfolio/app/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <nav>
       <ul>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/">home</NuxtLink></li>
         <li><NuxtLink to="/works">works</NuxtLink></li>
         <li><NuxtLink to="/profile">profile</NuxtLink></li>
         <li><NuxtLink to="/gallery">gallery</NuxtLink></li>
