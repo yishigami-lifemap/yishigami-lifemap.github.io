4bdba7b65ea188260d197f72805704870dfbc403
yishigami-lifemap <124244889+yishigami-lifemap@users.noreply.github.com>
2025-09-03 18:07:02 +0900

    cdnURL設定

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  app: {
+    cdnURL: "/yishigami-lifemap.github.io//",
+  },
+});
