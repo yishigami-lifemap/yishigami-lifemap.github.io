a655e755c55badf73baf0ce59f782d60d4630ca5
yishigami-lifemap <124244889+yishigami-lifemap@users.noreply.github.com>
2025-09-03 16:10:49 +0900

    docsの内容を更新

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   app: {
-    cdnURL: "https://yishigami-lifemap.github.io/nuxt4-portfolio/",
+    cdnURL: "/nuxt4-portfolio/",
   },
   ssr: false,
 });
