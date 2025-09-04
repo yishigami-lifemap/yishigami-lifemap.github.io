5a78b4f261ecdcd3e41cc58d4b550e8def718f2f
yishigami-lifemap <124244889+yishigami-lifemap@users.noreply.github.com>
2025-09-04 10:25:15 +0900

    Viewの用意

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    <small>copyright</small>
+  </footer>
+</template>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>Romanstein</header>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">home</NuxtLink></li>
+      <li><NuxtLink to="/about">about</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
