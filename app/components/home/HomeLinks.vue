7f96dbd0db70493491683e14c797b8c3f4215dfa
yishigami-lifemap <124244889+yishigami-lifemap@users.noreply.github.com>
2025-09-20 16:50:57 +0900

    トップ、linksのCSS調整

--- app/components/home/HomeLinks.vue
+++ app/components/home/HomeLinks.vue
@@ -31,46 +31,49 @@ const links: LinkItem[] = [
 
 <template>
   <section class="p-links">
-    <ul class="p-links__list">
-      <li v-for="link in links" :key="link.to">
-        <NuxtLink :to="link.to" class="p-links__anchor">
-          <dl class="p-links__item">
-            <dt class="p-links__head">
-              <p class="p-links__main">{{ link.en }}</p>
-              <p class="p-links__sub">{{ link.jp }}</p>
-            </dt>
-            <dd class="p-links__body">
-              <p class="p-links__description">{{ link.description }}</p>
-            </dd>
-          </dl>
-          <span class="p-links__arrow">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="12"
-              height="11.696"
-              viewBox="0 0 12 11.696"
-            >
-              <path
-                d="M5.1,3.431,5.7,2.836a.64.64,0,0,1,.908,0l5.207,5.2a.64.64,0,0,1,0,.908L6.6,14.154a.64.64,0,0,1-.908,0L5.1,13.56a.644.644,0,0,1,.011-.919L8.34,9.567H.643A.641.641,0,0,1,0,8.924V8.067a.641.641,0,0,1,.643-.643h7.7L5.113,4.349A.639.639,0,0,1,5.1,3.431Z"
-                transform="translate(0 -2.647)"
-                fill="rgba(135,135,135,0.4)"
-              />
-            </svg>
-          </span>
-        </NuxtLink>
-      </li>
-    </ul>
+    <div class="p-links__wrapper">
+      <ul class="p-links__list">
+        <li v-for="link in links" :key="link.to">
+          <NuxtLink :to="link.to" class="p-links__anchor">
+            <dl class="p-links__item">
+              <dt class="p-links__head">
+                <p class="p-links__main">{{ link.en }}</p>
+                <p class="p-links__sub">{{ link.jp }}</p>
+              </dt>
+              <dd class="p-links__body">
+                <p class="p-links__description">{{ link.description }}</p>
+              </dd>
+            </dl>
+            <span class="p-links__arrow">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="11.696"
+                viewBox="0 0 12 11.696"
+              >
+                <path
+                  d="M5.1,3.431,5.7,2.836a.64.64,0,0,1,.908,0l5.207,5.2a.64.64,0,0,1,0,.908L6.6,14.154a.64.64,0,0,1-.908,0L5.1,13.56a.644.644,0,0,1,.011-.919L8.34,9.567H.643A.641.641,0,0,1,0,8.924V8.067a.641.641,0,0,1,.643-.643h7.7L5.113,4.349A.639.639,0,0,1,5.1,3.431Z"
+                  transform="translate(0 -2.647)"
+                  fill="rgba(135,135,135,0.4)"
+                />
+              </svg>
+            </span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <style scoped>
 .p-links {
+  .p-links__wrapper {
+    container: p-links__wrapper / inline-size;
+  }
   .p-links__list {
     display: flex;
     flex-direction: column;
     gap: 8px;
-    container-type: inline-size;
-    container-name: linksList;
   }
   .p-links__anchor {
     position: relative;
@@ -87,7 +90,7 @@ const links: LinkItem[] = [
     display: grid;
     grid-template-columns: 140px 1fr;
     align-items: center;
-    padding: 4cqw 8cqw;
+    padding: 20px 24px;
   }
   .p-links__head {
     position: relative;
@@ -118,7 +121,7 @@ const links: LinkItem[] = [
     transform: translateY(-50%);
     transition: transform 0.24s var(--easing-outExpo);
   }
-  @container linksList (max-width: 580px) {
+  @container p-links__wrapper (max-width: 580px) {
     .p-links__head {
       display: flex;
       align-items: center;
@@ -131,11 +134,15 @@ const links: LinkItem[] = [
       font-size: 1.2rem;
     }
     .p-links__body {
-      margin-top: 1cqw;
+      margin-top: 0.4cqw;
       padding-left: 0;
     }
     .p-links__item {
       display: block;
+      padding: 4cqw 8.4cqw 5cqw;
+    }
+    .p-links__description {
+      font-size: 1.3rem;
     }
   }
 }
